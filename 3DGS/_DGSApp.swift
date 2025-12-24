91f676e8b1ccdc9fbaf6811db99bbaa4110823b8
siri-iii <siri-iii>
2025-12-24 10:45:13 +0800

    Initial Commit

---
+++ 3DGS/_DGSApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DGSApp.swift
+//  3DGS
+//
+//  Created by Siri- iii on 2025/12/24.
+//
+
+import SwiftUI
+
+@main
+struct _DGSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
